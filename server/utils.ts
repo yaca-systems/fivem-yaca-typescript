31b6f298e7380407cef3330cef34b7b2ef79ca7e
LuftigerLuca <l.schumacher@luftiger.eu>
2024-03-01 03:26:43 +0100

    started implementation of server-side functionality

---
+++ server/utils.ts
@@ -0,0 +1,44 @@
+import { YaCAServerModule } from "./yaca.server";
+
+/**
+ * Generates a random string of a given length.
+ *
+ * @param {number} [length=50] - The length of the string to generate. Defaults to 50 if not provided.
+ * @param {string} [possible="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"] - The characters to use in the string. Defaults to all alphanumeric characters if not provided.
+ * @returns {string} The generated random string.
+ */
+function generateRandomString(
+  length: number = 50,
+  possible: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+): string {
+  let text = "";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+/**
+ * Generate a random name and insert it into the database.
+ *
+ * @param src The ID of the player.
+ */
+export function generateRandomName(src: string): string | undefined {
+  let name: string | undefined;
+  for (let i = 0; i < 10; i++) {
+    const generatedName = generateRandomString(15);
+    if (!YaCAServerModule.nameSet.has(generatedName)) {
+      name = generatedName;
+      YaCAServerModule.nameSet.add(generatedName);
+      break;
+    }
+  }
+
+  if (!name) {
+    console.error(
+      `YaCA: Couldn't generate a random name for player ${GetPlayerName(src)} (ID: ${src}).`,
+    );
+  }
+
+  return name;
+}
--- server/yaca.server.ts
+++ server/yaca.server.ts
@@ -1,9 +1,35 @@
 import { initLocale } from "@overextended/ox_lib/server";
+import { generateRandomName } from "./utils";
 
 initLocale();
 
+export interface YaCAPlayer {
+  voiceSettings: {
+    voiceRange: number;
+    voiceFirstConnect: boolean;
+    forceMuted: boolean;
+    ingameName: string;
+    mutedOnPhone: boolean;
+  };
+}
+
+export interface YaCAServerSettings {
+  uniqueServerId: string; // Unique TeamSpeak-Server ID
+  ingameChannelId: number; // Channel ID of the ingame channel
+  ingameChannelPassword: string; // Password of the ingame channel
+  defaultChannelId: number; // efault Teamspeak Channel, if player can't be moved back to his old channel
+  useWhisper: boolean; // If true, it will use the teamspeak whisper system
+  excludeChannels: number[]; // Channel ID's where users can be in while being ingame and won't be moved into the ingame channel
+}
+
 export class YaCAServerModule {
   static instance: YaCAServerModule;
+  static nameSet: Set<string> = new Set();
+  static players: Map<string, YaCAPlayer> = new Map();
+
+  constructor() {
+    this.registerEvents();
+  }
 
   /**
    * Gets the singleton of YaCAServerModule.
@@ -16,5 +42,60 @@ export class YaCAServerModule {
     }
 
     return this.instance;
+  }
+
+  /**
+   * Initialize the player on first connect.
+   *
+   * @param {string} source - The source-id of the player to connect
+   */
+  connectToVoice(source: string) {
+    if (!source) return;
+
+    const name: string | undefined = generateRandomName(source);
+    if (!name) return;
+
+    YaCAServerModule.players.set(source, {
+      voiceSettings: {
+        voiceRange: 25, //TODO: Change this to a config value
+        voiceFirstConnect: true,
+        forceMuted: false,
+        ingameName: name,
+        mutedOnPhone: false,
+      },
+    });
+
+    this.connect(source);
+  }
+
+  /**
+   * Sends initial data needed to connect to teamspeak plugin.
+   *
+   * @param {string }source - The source-id of the player to connect
+   */
+  connect(source: string) {
+    const player = YaCAServerModule.players.get(source);
+    if (!player) {
+      console.error(`YaCA: Missing player data for ${source}.`);
+      return;
+    }
+
+    player.voiceSettings.voiceFirstConnect = true;
+
+    //TODO: Change this to a config value
+    const serverSettings: YaCAServerSettings = {
+      uniqueServerId: "1",
+      ingameChannelId: 1,
+      ingameChannelPassword: "password",
+      defaultChannelId: 1,
+      useWhisper: true,
+      excludeChannels: [1, 2, 3],
+    };
+
+    TriggerClientEvent("client:yaca:init", source, serverSettings);
+  }
+
+  registerEvents() {
+    onNet("server:yaca:nuiReady", this.connectToVoice);
   }
 }
