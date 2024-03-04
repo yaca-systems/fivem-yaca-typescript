0e5acb284a5552389f445a424d639e11476587ed
MineMalox <55745413+minemalox@users.noreply.github.com>
2024-03-05 00:59:07 +0100

    fixed loading of client module

--- client/index.ts
+++ client/index.ts
@@ -1,9 +1,3 @@
 import { YaCAClientModule } from "yaca";
 
-on("onClientResourceStart", (resourceName: string) => {
-  if (GetCurrentResourceName() !== resourceName) {
-    return;
-  }
-
-  new YaCAClientModule();
-});
+new YaCAClientModule();
