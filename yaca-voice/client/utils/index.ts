ef26873a5398d1509121a0015c3b8ced99234997
minemalox <55745413+minemalox@users.noreply.github.com>
2024-05-19 08:51:38 +0200

    feat(main): round coords to 2 digits after decimal point
    Signed-off-by: minemalox <55745413+minemalox@users.noreply.github.com>

--- yaca-voice/client/utils/index.ts
+++ yaca-voice/client/utils/index.ts
@@ -19,7 +19,22 @@ export function clamp(value: number, min = 0, max = 1) {
 }
 
 /**
+ * Rounds a float to a specified number of decimal places.
+ * Defaults to 2 decimal places if not provided.
+ *
+ * @param {number} num - The number to round.
+ * @param {number} decimalPlaces - The number of decimal places to round to.
+ *
+ * @returns {number} The rounded number.
+ */
+export function roundFloat(num: number, decimalPlaces: number = 2): number {
+  return parseFloat(num.toFixed(decimalPlaces));
+}
+
+/**
  * Convert camera rotation to direction vector.
+ *
+ * @returns {x: number, y: number, z: number} The direction vector.
  */
 export function getCamDirection(): { x: number; y: number; z: number } {
   const rotVector = GetGameplayCamRot(0),
@@ -28,8 +43,8 @@ export function getCamDirection(): { x: number; y: number; z: number } {
     num3 = Math.abs(Math.cos(num2));
 
   return {
-    x: -Math.sin(num) * num3,
-    y: Math.cos(num) * num3,
-    z: GetEntityForwardVector(cache.ped)[2],
+    x: roundFloat(-Math.sin(num) * num3),
+    y: roundFloat(Math.cos(num) * num3),
+    z: roundFloat(GetEntityForwardVector(cache.ped)[2]),
   };
 }
