fd7cc1d51f1885729cc8e9069ca4213cfd75e654
Blue <florian.uaz@web.de>
2024-06-09 13:13:01 +0200

    fix(initreques): empty password was not possible to use

--- yaca-voice/client/yaca/main.ts
+++ yaca-voice/client/yaca/main.ts
@@ -446,7 +446,7 @@ export class YaCAClientModule {
    * @param {DataObject} dataObj - The data object to initialize the plugin with.
    */
   initRequest(dataObj: DataObject) {
-    if (!dataObj || !dataObj.suid || typeof dataObj.chid !== "number" || !dataObj.deChid || !dataObj.ingameName || !dataObj.channelPassword) {
+    if (!dataObj || !dataObj.suid || typeof dataObj.chid !== "number" || !dataObj.deChid || !dataObj.ingameName || typeof dataObj.channelPassword == "undefined") {
       console.log("[YACA-Websocket]: Error while initializing plugin");
       this.notification(locale("connect_error"), YacaNotificationType.ERROR);
       return;
