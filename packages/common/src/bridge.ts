bcf11e58953dd66b6be4fa5c5c60c303b78edeb8
Malox <55745413+minemalox@users.noreply.github.com>
2024-11-16 22:53:39 +0100

    chore(docs): added docs to salty chat bridge export
    Signed-off-by: Malox <55745413+minemalox@users.noreply.github.com>

--- packages/common/src/bridge.ts
+++ packages/common/src/bridge.ts
@@ -1,3 +1,9 @@
+/**
+ * Add an export in the saltychat namespace.
+ *
+ * @param method - the export method name
+ * @param cb - the callback to execute
+ */
 export function saltyChatExport(method: string, cb: (...args: never[]) => void) {
   on(`__cfx_export_saltychat_${method}`, (setCb: (...args: unknown[]) => void) => {
     setCb(cb)
