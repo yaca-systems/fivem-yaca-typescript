3b9991d8fc799e6fe912391e9d09b4fe5930d391
Malox <55745413+minemalox@users.noreply.github.com>
2025-01-18 14:55:28 +0100

    fix(events): fixed native error on empty arrays
    Signed-off-by: Malox <55745413+minemalox@users.noreply.github.com>

--- apps/yaca-server/src/utils/events.ts
+++ apps/yaca-server/src/utils/events.ts
@@ -1,14 +1,16 @@
 export const triggerClientEvent = (eventName: string, targetIds: number[] | number, ...args: unknown[]) => {
-  // @ts-expect-error - This is a valid use case for the `source` variable
-  const dataSerialized = msgpack_pack(args)
+  if (!Array.isArray(targetIds)) {
+    targetIds = [targetIds]
+  }
 
-  if (Array.isArray(targetIds)) {
-    for (const targetId of targetIds) {
-      TriggerClientEventInternal(eventName, targetId.toString(), dataSerialized, dataSerialized.length)
-    }
-
+  if (targetIds.length < 1) {
     return
   }
 
-  TriggerClientEventInternal(eventName, targetIds.toString(), dataSerialized, dataSerialized.length)
+  // @ts-expect-error - msgpack_pack is not typed but available in the global scope.
+  const dataSerialized = msgpack_pack(args)
+
+  for (const targetId of targetIds) {
+    TriggerClientEventInternal(eventName, targetId.toString(), dataSerialized, dataSerialized.length)
+  }
 }
