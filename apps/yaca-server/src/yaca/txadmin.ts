7937ebd440957d08f6f902c0ad26f7777783a5dc
Malox <55745413+minemalox@users.noreply.github.com>
2025-01-29 23:04:01 +0100

    refactor: fixed some deepsource occurrences
    Signed-off-by: Malox <55745413+minemalox@users.noreply.github.com>

--- apps/yaca-server/src/yaca/txadmin.ts
+++ apps/yaca-server/src/yaca/txadmin.ts
@@ -1,5 +1,3 @@
-import { triggerClientEvent } from 'src/utils/events.js'
-
 /**
  * The txadmin module for the server.
  */
@@ -8,18 +6,11 @@ export class YaCAServerTxAdminModule {
    * Creates an instance of the txadmin module.
    */
   constructor() {
-    this.registerEvents()
-  }
-
-  /**
-   * Register server events.
-   */
-  registerEvents() {
     /**
      * Handles the "txsv:req:spectate:end" event.
      */
     onNet('txsv:req:spectate:end', () => {
-      triggerClientEvent('client:yaca:txadmin:stopspectate', source)
+      emitNet('client:yaca:txadmin:stopspectate', source)
     })
   }
 }
