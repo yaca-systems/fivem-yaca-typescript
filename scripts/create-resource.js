d77a3c95d72afd0edd9d680f24c77bb1afe9da00
Malox <55745413+minemalox@users.noreply.github.com>
2025-02-17 18:21:12 +0100

    chore: use node 22
    Signed-off-by: Malox <55745413+minemalox@users.noreply.github.com>

--- scripts/create-resource.js
+++ scripts/create-resource.js
@@ -24,6 +24,7 @@ writeFileSync(
     `fx_version 'cerulean'
 games { 'gta5', 'rdr3' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+node_version '22'
 
 name '${packageJson.name}'
 author '${packageJson.author}'
@@ -32,7 +33,7 @@ repository '${packageJson.repository.url}'
 description '${packageJson.description}'
 
 dependencies {
-    '/server:7290',
+    '/server:12913',
     '/onesync',
 }
 
